90c959747ee6adc175eeb486c467bfe62af52b98
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-09-10 20:18:39 +0800

    Update Python-Notepad.py

--- Python-Notepad.py
+++ Python-Notepad.py
@@ -5,7 +5,8 @@ class MyNotepad:
     
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("600x750")
+        self.root.geometry("600x800")
+        self.root.minsize(150,150)
         self.root.title("My Notepad")
         self.root.configure(bg='lightblue')
         self.char_count = tk.Frame(self.root,bd=1,relief='sunken')
@@ -21,87 +22,113 @@ class MyNotepad:
         self.char_count_label.pack(fill='x')
 
         self.current_font = 'Arial'
-        self.font_size = 12
-        self.fonts = ['Arial','Courier New', 'Comic Sans MS', 'Times New Roman', 'Impact', 'MS Sans Serif', 'Roman', 'System']
+        self.font_size = 14
+        self.font_weight = 'normal'
+        # self.fonts = ['Arial','Courier New', 'Comic Sans MS', 'Times New Roman', 'Impact', 'MS Sans Serif', 'Roman', 'System']
         
         self.menubar = tk.Menu(self.root)
         self.filemenu = tk.Menu(self.menubar, tearoff=0, font=('Arial',12))
         self.actionmenu = tk.Menu(self.menubar, tearoff=0, font=('Arial',12))
-        self.fontmenu = tk.Menu(self.actionmenu,tearoff=0, font=('Arial',12))
+        self.fontmenu = tk.Menu(self.menubar,tearoff=0, font=('Arial',12))
+        self.fontsmenu = tk.Menu(self.fontmenu,tearoff=0, font=('Arial',12))
         self.fontsizesmenu = tk.Menu(self.fontmenu,tearoff=0, font=('Arial',12))
+        self.fontweightmenu = tk.Menu(self.fontmenu,tearoff=0, font=('Arial',12))
         self.settingsmenu = tk.Menu(self.menubar, tearoff=0, font=('Arial',12))
-        self.actionmenu.add_cascade(menu=self.fontmenu, label="Fonts")
-        self.fontmenu.add_cascade(menu=self.fontsizesmenu, label="Font sizes")
-        self.filemenu.add_command(label="Open file", command=self.open_file)
-        self.filemenu.add_command(label="Save as...", command=self.save_as_text_file)
-        self.filemenu.add_command(label="Close", command=self.on_closing)
-        self.actionmenu.add_command(label="Copy to clipboard", command=self.save_all_to_clipboard)
-        self.actionmenu.add_command(label="Paste from clipboard", command=self.paste_from_clipboard)
+        self.fontmenu.add_cascade(menu=self.fontsizesmenu, label="Font Sizes")
+        self.fontmenu.add_cascade(menu=self.fontweightmenu, label="Font Weights")
+        self.fontmenu.add_cascade(menu=self.fontsmenu, label="Fonts")
+        self.filemenu.add_command(label="Open File", command=self.open_file)
+        self.filemenu.add_command(label="Save As...", command=self.save_as_text_file)
+        self.filemenu.add_command(label="Close Window", command=self.on_closing)
+        self.actionmenu.add_command(label="Copy to Clipboard", command=self.save_all_to_clipboard)
+        self.actionmenu.add_command(label="Paste from Clipboard", command=self.paste_from_clipboard)
         self.actionmenu.add_command(label="Clear Notepad", command=self.clear)
+        self.actionmenu.add_command(label="Highlight", command= self.highlight)
+        self.actionmenu.add_command(label="Remove Highlight", command= self.remove_highlight)
         self.actionmenu.add_separator()
         self.actionmenu.add_command(label="Set all text to lowercase", command=self.set_all_to_lowercase)
-        self.actionmenu.add_command(label="Set all text to uppercase", command=self.set_all_to_uppercase)
-        self.settingsmenu.add_checkbutton(label="Show character count", command=self.character_count,variable=self.char_count_checked)
-        self.settingsmenu.add_checkbutton(label="Add canvas", command=self.draw_space,variable=self.draw_space_checked)
+        self.actionmenu.add_command(label="Set all text to UPPERCASE", command=self.set_all_to_uppercase)
+        self.settingsmenu.add_checkbutton(label="Show Character Count", command=self.character_count,variable=self.char_count_checked)
+        self.settingsmenu.add_checkbutton(label="Add Canvas", command=self.create_draw_space,variable=self.draw_space_checked)
         self.menubar.add_cascade(menu=self.filemenu, label="File")
         self.menubar.add_cascade(menu=self.actionmenu, label="Actions")
+        self.menubar.add_cascade(menu=self.fontmenu, label='Fonts')
         self.menubar.add_cascade(menu=self.settingsmenu, label='Settings')
-        
-        # for font in self.fonts:
+                # for font in self.fonts:
         #     self.fontmenu.add_command(label=font,font=(font,12),command=lambda: self.set_new_font(font))
-        self.fontmenu.add_command(label='Change font size',font=('Arial',12))
-        self.fontsizesmenu.add_command(label='Font size: 16',font=('Arial',16),command=lambda: self.set_new_font_size(16))
-        self.fontsizesmenu.add_command(label='Font size: 18',font=('Arial',18),command=lambda: self.set_new_font_size(18))
-        self.fontsizesmenu.add_command(label='Font size: 20',font=('Arial',20),command=lambda: self.set_new_font_size(20))
-        self.fontsizesmenu.add_command(label='Font size: 22',font=('Arial',22),command=lambda: self.set_new_font_size(22))
-        self.fontsizesmenu.add_command(label='Font size: 24',font=('Arial',24),command=lambda: self.set_new_font_size(24))
-        self.fontsizesmenu.add_command(label='Font size: 30',font=('Arial',30),command=lambda: self.set_new_font_size(30))
-        self.fontsizesmenu.add_command(label='Font size: 36',font=('Arial',36),command=lambda: self.set_new_font_size(36))
-        self.fontmenu.add_command(label='Arial',font=('Arial',12),command=lambda: self.set_new_font('Arial'))
-        self.fontmenu.add_command(label='Courier New',font=('Courier New',12),command=lambda: self.set_new_font('Courier New'))
-        self.fontmenu.add_command(label='Comic Sans MS',font=('Comic Sans MS',12),command=lambda: self.set_new_font('Comic Sans MS'))
-        self.fontmenu.add_command(label='Times New Roman',font=('Times New Roman',12),command=lambda: self.set_new_font('Times New Roman'))
-        self.fontmenu.add_command(label='Impact',font=('Impact',12),command=lambda: self.set_new_font('Impact'))
-        self.fontmenu.add_command(label='MS Sans Serif',font=('MS Sans Serif',12),command=lambda: self.set_new_font('MS Sans Serif'))
-        self.fontmenu.add_command(label='Roman',font=('Roman',12),command=lambda: self.set_new_font('Roman'))
-        self.fontmenu.add_command(label='System',font=('System',12),command=lambda: self.set_new_font('System'))
+        self.fontsizesmenu.add_command(label='16',font=('Arial',16),command=lambda: self.set_new_font_size(16))
+        self.fontsizesmenu.add_command(label='18',font=('Arial',18),command=lambda: self.set_new_font_size(18))
+        self.fontsizesmenu.add_command(label='20',font=('Arial',20),command=lambda: self.set_new_font_size(20))
+        self.fontsizesmenu.add_command(label='22',font=('Arial',22),command=lambda: self.set_new_font_size(22))
+        self.fontsizesmenu.add_command(label='24',font=('Arial',24),command=lambda: self.set_new_font_size(24))
+        self.fontsizesmenu.add_command(label='30',font=('Arial',30),command=lambda: self.set_new_font_size(30))
+        self.fontsizesmenu.add_command(label='36',font=('Arial',36),command=lambda: self.set_new_font_size(36))
+        self.fontweightmenu.add_command(label='Normal',font=('Arial',12, 'normal'),command=lambda: self.set_new_font_weight('normal'))
+        self.fontweightmenu.add_command(label='Italic',font=('Arial',12, 'italic'),command=lambda: self.set_new_font_weight('italic'))
+        self.fontweightmenu.add_command(label='Bold',font=('Arial',12, 'bold'),command=lambda: self.set_new_font_weight('bold'))
+        self.fontweightmenu.add_command(label='Bold + Italic',font=('Arial',12, 'bold italic'),command=lambda: self.set_new_font_weight('bold italic'))
+        self.fontsmenu.add_command(label='Arial',font=('Arial',12),command=lambda: self.set_new_font('Arial'))
+        self.fontsmenu.add_command(label='Courier New',font=('Courier New',12),command=lambda: self.set_new_font('Courier New'))
+        self.fontsmenu.add_command(label='Comic Sans MS',font=('Comic Sans MS',12),command=lambda: self.set_new_font('Comic Sans MS'))
+        self.fontsmenu.add_command(label='Times New Roman',font=('Times New Roman',12),command=lambda: self.set_new_font('Times New Roman'))
+        self.fontsmenu.add_command(label='Impact',font=('Impact',12),command=lambda: self.set_new_font('Impact'))
+        self.fontsmenu.add_command(label='MS Sans Serif',font=('MS Sans Serif',12),command=lambda: self.set_new_font('MS Sans Serif'))
+        self.fontsmenu.add_command(label='Roman',font=('Roman',12),command=lambda: self.set_new_font('Roman'))
+        self.fontsmenu.add_command(label='System',font=('System',12),command=lambda: self.set_new_font('System'))
         
         self.root.config(menu=self.menubar)
         
-        self.textbox = tk.Text(self.root,font=(self.current_font))
+        self.textbox = tk.Text(self.root,font=(self.current_font,self.font_size,self.font_weight))
         self.textbox.pack(padx=10,pady=5,expand=True,fill="both")
 
         self.root.after(0, self.check_state)
-        
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
     
-    def draw_space(self):
+    def highlight(self):
+        try:
+            start = self.textbox.index("sel.first")
+            end = self.textbox.index("sel.last")
+            self.textbox.tag_add("sel_txt",start,end)
+            self.textbox.tag_config('sel_txt', background='yellow',foreground='black')
+        except:
+            messagebox.showwarning(message="No text was selected for highlighting.")
+            
+    def remove_highlight(self):
+        try:
+            start = self.textbox.index("sel.first")
+            end = self.textbox.index("sel.last")
+            #used to get selected text
+            self.textbox.tag_remove("sel_txt",start,end)
+        except:
+            messagebox.showwarning(message="No text was selected to remove highlighting.")
+      
+    def create_draw_space(self):
         if self.draw_space_checked.get() == 1:
             self.startx, self.starty = 0, 0
             self.endx, self.endy = 0, 0
-            self.draw_space = tk.Canvas(self.root)
+            self.draw_space = tk.Canvas(self.root, bg='white')
             self.draw_space.pack(padx=10,pady=10)
-            self.draw_space.bind('<Button>', self.get_click)
-            self.draw_space.bind('<ButtonRelease>', self.release_click)
+            self.brush_size = 3
+            self.draw_space.bind('<B1-Motion>', self.draw)
+            self.draw_space.bind('<Button-3>', self.erase)
         else:
             self.draw_space.pack_forget()
 
-    def get_click(self,event):
-        self.startx = event.x
-        self.starty = event.y
-
-    def release_click(self,event):
-        self.endx = event.x
-        self.endy = event.y
-        self.draw_space.create_line(self.startx, self.starty, self.endx, self.endy, fill='blue')
-
+    def draw(self, event):
+        x = event.x
+        y = event.y
+        self.draw_space.create_oval((x-self.brush_size/2,y-self.brush_size/2,x+self.brush_size/2,y+self.brush_size/2), fill='black')
+        
+    def erase(self, event):
+        self.draw_space.delete('all')
+        
     def check_state(self):
         if self.char_count_checked.get() == 1:
           self.character_count()
         else:
             self.char_count_var.set("Character count: Disabled")
-        self.root.after(2200,self.check_state)
+        self.root.after(2300,self.check_state)
 
     def character_count(self):
         text = self.textbox.get('1.0',tk.END)
@@ -142,11 +169,15 @@ class MyNotepad:
     
     def set_new_font(self,font):
         self.current_font = font
-        self.textbox.configure(font=(self.current_font,self.font_size))
+        self.textbox.configure(font=(self.current_font,self.font_size,self.font_weight))
     
     def set_new_font_size(self,size):
         self.font_size = size
-        self.textbox.configure(font=(self.current_font,self.font_size))
+        self.textbox.configure(font=(self.current_font,self.font_size,self.font_weight))
+        
+    def set_new_font_weight(self,weight):
+        self.font_weight = weight
+        self.textbox.configure(font=(self.current_font,self.font_size,self.font_weight))
 
     def set_all_to_lowercase(self):
         text_to_lower = self.textbox.get('1.0',tk.END)
