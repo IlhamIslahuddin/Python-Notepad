181cab6edfd11cd74ac994754cc8668b62d67ba7
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-09-10 23:25:29 +0800

    Update Python-Notepad.py
    -Added option to change window background colour

--- Python-Notepad.py
+++ Python-Notepad.py
@@ -17,7 +17,7 @@ class MyNotepad:
         self.draw_space_var = tk.StringVar()
         self.draw_space_checked = tk.IntVar()
 
-        self.char_count_label = tk.Label(self.char_count, textvariable=self.char_count_var, anchor='w',padx=5,pady=5, font=('Arial',12,'italic))
+        self.char_count_label = tk.Label(self.char_count, textvariable=self.char_count_var, anchor='w',padx=5,pady=5, font=('Arial',12,'italic'))
         self.char_count_label.pack(fill='x')
 
         #default font settings
@@ -33,9 +33,11 @@ class MyNotepad:
         self.fontsizesmenu = tk.Menu(self.fontmenu,tearoff=0, font=('Arial',12))
         self.fontweightmenu = tk.Menu(self.fontmenu,tearoff=0, font=('Arial',12))
         self.settingsmenu = tk.Menu(self.menubar, tearoff=0, font=('Arial',12))
+        self.backgroundmenu = tk.Menu(self.settingsmenu, tearoff=0, font=('Arial',12))
         self.fontmenu.add_cascade(menu=self.fontsizesmenu, label="Font Sizes")
         self.fontmenu.add_cascade(menu=self.fontweightmenu, label="Font Weights")
         self.fontmenu.add_cascade(menu=self.fontsmenu, label="Fonts")
+        self.settingsmenu.add_cascade(menu=self.backgroundmenu,label="Change background colour")
         self.filemenu.add_command(label="Open File", command=self.open_file, accelerator="Ctrl O")
         self.filemenu.add_command(label="Save As...", command=self.save_as_text_file, accelerator="Ctrl S")
         self.filemenu.add_command(label="Close Window", command=self.on_closing, accelerator="Alt F4")
@@ -49,6 +51,10 @@ class MyNotepad:
         self.actionmenu.add_command(label="Set all text to UPPERCASE", command=self.set_all_to_uppercase)
         self.settingsmenu.add_checkbutton(label="Show Character Count", command=self.character_count,variable=self.char_count_checked)
         self.settingsmenu.add_checkbutton(label="Add Canvas", command=self.create_draw_space,variable=self.draw_space_checked)
+        self.backgroundmenu.add_command(label="Light Blue", command=lambda: self.change_background("lightblue"))
+        self.backgroundmenu.add_command(label="Light Yellow", command=lambda: self.change_background("lightyellow"))
+        self.backgroundmenu.add_command(label="Red", command=lambda: self.change_background("red"))
+        self.backgroundmenu.add_command(label="Light Green", command=lambda: self.change_background("lightgreen"))
         self.menubar.add_cascade(menu=self.filemenu, label="File")
         self.menubar.add_cascade(menu=self.actionmenu, label="Actions")
         self.menubar.add_cascade(menu=self.fontmenu, label='Fonts')
@@ -77,7 +83,7 @@ class MyNotepad:
         self.root.config(menu=self.menubar)
         
         self.textbox = tk.Text(self.root,font=(self.current_font,self.font_size,self.font_weight))
-        self.textbox.pack(padx=10,pady=5,expand=True,fill="both")
+        self.textbox.pack(padx=10,pady=10,expand=True,fill="both")
         
         #shortcuts
         self.textbox.bind("<Control-{>",self.highlight)
@@ -89,7 +95,10 @@ class MyNotepad:
         self.root.after(0, self.check_state)
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
-    
+        
+    def change_background(self, colour):
+        self.root.configure(bg=colour)
+
     def highlight(self,event=""):
         #event = "" sets default value for event so it is not always needed as an arg
         try:
@@ -114,7 +123,7 @@ class MyNotepad:
             self.startx, self.starty = 0, 0
             self.endx, self.endy = 0, 0
             self.draw_space = tk.Canvas(self.root, bg='white')
-            self.draw_space.pack(padx=10,pady=10)
+            self.draw_space.pack(padx=10,pady=10,fill="both")
             self.brush_size = 3
             self.draw_space.bind('<B1-Motion>', self.draw)
             self.draw_space.bind('<Button-3>', self.erase)
